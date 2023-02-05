8443a8f1c372cbd8a3e8101b85c3c14cded6f813
senthilkumar-m16 <123889574+senthilkumar-m16@users.noreply.github.com>
2023-02-05 18:26:07 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -29,6 +29,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it in the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+streamlit.stop()
+
 
 import snowflake.connector
 
