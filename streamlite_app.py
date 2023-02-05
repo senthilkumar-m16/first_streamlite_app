bc3c990174ad41e29ac4d8a08c7cba297515859d
senthilkumar-m16 <123889574+senthilkumar-m16@users.noreply.github.com>
2023-02-05 20:12:57 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -57,17 +57,18 @@ def get_fruit_load_list():
 	     return my_cur.fetchall()
 
 #add a button to load the fruit
-#if streamlit.button('Get Fruit Load List'):
-#	my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#	my_data_rows = get_fruit_load_list()
-#	streamlit.dataframe(my_data_rows)
+if streamlit.button('Get Fruit Load List'):
+	my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+	my_data_rows = get_fruit_load_list()
+        my_cnx.close()
+        streamlit.dataframe(my_data_rows)
 
 # Add a button to load the fruit 
-if steramlit.button('Get Fruit List'):
-      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-      my_data_rows = get_fruit_load_list()
-      my_cnx.close()
-      streamlit.dataframe(my_data_rows)
+#if streamlit.button('Get Fruit Load List'):
+#      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#      my_data_rows = get_fruit_load_list()
+#     my_cnx.close()
+#      streamlit.dataframe(my_data_rows)
 
 #add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 #streamlit.write('The user entered ', add_my_fruit)
