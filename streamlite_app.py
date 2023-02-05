5f010890715732ef1d62f5f461d82b73ff013000
senthilkumar-m16 <123889574+senthilkumar-m16@users.noreply.github.com>
2023-02-05 17:25:21 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -8,3 +8,28 @@ streamlit.text('Hard Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+fruits_selected = streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+streamlit.dataframe(fruits_to_show)
+
+#New Section to display Fruitvist API
+streamlit.header("Fruityvice Fruit Advice!")
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_response)
+# take json verson of the response and normalize it 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#output it in the screen as a table
+streamlit.dataframe(fruityvice_normalized)
+
+
+import snowflake.conector
+
